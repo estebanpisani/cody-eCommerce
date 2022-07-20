ba2687bf75b46bc676515e603a31f2738dd112aa
sofia <malenamayer16@gmail.com>
2022-07-20 15:36:56 -0300

    modal

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,50 +1,87 @@
 import React from 'react'
 import '../styles/products.css'
 import { Input } from "@material-tailwind/react";
-
+import BasicModal from './modal';
 
 
 const products = [
     {
       id: 1,
       name: 'Basic Tee',
+      description:'El café expreso es una forma de preparación de café originada en Italia.​ Debe su término a la obtención de esta bebida a través de una cafetera expreso.​',
       href: '#',
       imageSrc: 'https://imgur.com/rMuohfR.png',
       imageAlt: "Front of men's Basic Tee in black.",
       price: '$35',
       color: 'Black',
+      stock:20,
+      categories: [
+          'desayuno', 
+          'merienda'
+      ],
+      variations:[
+           'frappuccino', 'naranja', 'frutilla', 'manzana'
+      ]
     },
     {
     id: 2,
     name: 'Cheesecake',
-    href: '#',
+    description:'El café expreso es una forma de preparación de café originada en Italia.​ Debe su término a la obtención de esta bebida a través de una cafetera expreso.​',    href: '#',
     imageSrc: 'https://i.imgur.com/0C7ptLx.png',
     imageAlt: 'cheesecake',
     price: '$50',
-    color: 'green'
+    color: 'green',
+    stock:20,
+    categories: [
+        'desayuno', 
+        'merienda'
+    ],
+    variations:[
+         'frappuccino', 'naranja', 'frutilla', 'manzana'
+    ]
     },
     {
         id: 3,
         name: 'Strawberrycake',
+        description:'El café expreso es una forma de preparación de café originada en Italia.​ Debe su término a la obtención de esta bebida a través de una cafetera expreso.​',
         href: '#',
         imageSrc: 'https://i.imgur.com/yTalbip.png',
         imageAlt: 'cheesecake',
         price: '$50',
-        color: 'green'
+        color: 'green',
+        stock:20,
+        categories: [
+       'desayuno', 
+        'merienda'
+        ],
+        variations:[
+        'frappuccino', 'naranja', 'frutilla', 'manzana'
+        ]
         },
         {
             id: 3,
             name: 'Strawberrycake',
+            description:'El café expreso es una forma de preparación de café originada en Italia.​ Debe su término a la obtención de esta bebida a través de una cafetera expreso.​',
             href: '#',
             imageSrc: 'https://i.imgur.com/yTalbip.png',
             imageAlt: 'cheesecake',
             price: '$50',
-            color: 'green'
-            }
+            color: 'green',
+            stock:20,
+                categories: [
+                    'desayuno', 
+                    'merienda'
+                ],
+                variations:[
+                     'frappuccino', 'naranja', 'frutilla', 'manzana'
+                ]
+        }
     // More products...
   ]
-  
+  console.log(products)
+
   export default function Products() {
+  
     return (
       <div className="bg-white">
         
@@ -55,8 +92,8 @@ const products = [
           <div className="mt-6 flex flex-wrap grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
           
             {products.map((product) => (
-              <div key={product.id} className="group relative group-cards">
-                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none card-container">
+              <div key={product.id} className="group relative group-cards">      
+<div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none card-container">
                   <div className='imgcontainer'>
                   <img
                     src={product.imageSrc}
@@ -66,20 +103,19 @@ const products = [
                   </div>
                   <div className='info-content'>
                   <h3 className="text-sm text-white fontfamily">
-                      <a href={product.href}>
+                      
                         <span aria-hidden="true" className="absolute inset-0" />
                         {product.name}
-                      </a>
+                  
                   </h3>
                   <div className='product-info'>
                         <p className="text-sm text-white">{product.color}</p>
                         <p className="price text-sm font-medium text-white">{product.price}</p>
                   </div>
+                
                   <div>
-                  <button class="button hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-500 hover:border-blue-500 rounded">
-                        Ver más
-                  </button>
-                        
+                
+               
                   </div>
                   </div>
                 </div>
@@ -90,11 +126,16 @@ const products = [
                   </div>
                   
                 </div>
+                <div className='detail-modal'> <BasicModal data={product}></BasicModal></div>
+               
               </div>
             ))}
           </div>
-        </div>
-      </div>
+        </div>   
+    
+      </div>     
+      
+   
     )
   }
   
