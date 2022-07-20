12790f41bad45f885cb1e36dcd7ca26605818f1b
kitolrc <estebanfortini@gmail.com>
2022-07-20 16:00:05 -0300

    20/0715:59

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -30,7 +30,7 @@ export default function Index() {
                                             <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
                                                 <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white">Fecha</h2>
                                                 <div className="flex h-full items-end pb-6">
-                                                    <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"><ModalEvent /></h3>
+                                                    <ModalEvent />
                                                 </div>
                                             </div>
                                         </div>
@@ -182,9 +182,10 @@ export default function Index() {
                                         <div className="flex flex-shrink-0 relative w-full sm:w-auto">
                                             <img src="https://i.ibb.co/fDngH9G/carosel-1.png" alt="black chair and white table" className="object-cover object-center w-full" />
                                             <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
-                                                <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white">Catalog 1</h2>
+                                                <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white">Fecha</h2>
                                                 <div className="flex h-full items-end pb-6">
-                                                    <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white">Minimal Interior</h3>
+                                                    {/* <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"></h3> */}
+                                                    <ModalEvent />
                                                 </div>
                                             </div>
                                         </div>
@@ -336,9 +337,10 @@ export default function Index() {
                                         <div className="flex flex-shrink-0 relative w-full sm:w-auto">
                                             <img src="https://i.ibb.co/fDngH9G/carosel-1.png" alt="black chair and white table" className="object-cover object-center w-full" />
                                             <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
-                                                <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white">Catalog 1</h2>
+                                                <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white">Fecha</h2>
                                                 <div className="flex h-full items-end pb-6">
-                                                    <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white">Minimal Interior</h3>
+                                                    {/* <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"></h3> */}
+                                                    <ModalEvent />
                                                 </div>
                                             </div>
                                         </div>
---
+++ frontend/src/components/Comments.jsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+export default function Comments() {
+  return (
+    <div className='background-accordion-comments'>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+          sx={{backgroundColor:'#FFA43A'}}
+        >
+          <Typography>Comments</Typography>
+        </AccordionSummary>
+        <AccordionDetails
+         sx={{backgroundColor:'#F9F2ED'}}
+         >
+          <Typography>
+            {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. */}
+            Aca ademtro va la funcion de los comentarios pero si la pongo se me rompe todo.
+            
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      {/* <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion disabled>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography>Disabled Accordion</Typography>
+        </AccordionSummary>
+      </Accordion> */}
+    </div>
+  );
+}
