936f17c74368516af492ffdfe53c8b76be7e6b5e
Esteban Pisani <estebandpisani@gmail.com>
2022-07-20 15:08:16 -0300

    Campo imageAlt agregado

--- models/product.js
+++ models/product.js
@@ -4,7 +4,8 @@ const productSchema = new mongoose.Schema(
     {
         name:{ type:String, require:true },
         description: { type:String, require:false},
-        images: [{type:String, require:false}],
+        image: {type:String, require:false},
+        imageAlt: {type:String, require:false},
         price: {type:Number, require:true, default: 0},
         stock: {type:Number, require: true, default: 0},
         date: {type:Date, default: Date(), require: true},
