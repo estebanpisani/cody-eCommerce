edc14324cf5e6c6e73525c307acf6bf69b5712e0
martin.ricciardi.04@gmail.com <martin.ricciardi.04@gmail.com>
2022-07-20 22:37:40 -0300

    login skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Login from './pages/Login'
+import SignUp from './pages/SignUp';
 // import Hero from '../src/components/Hero'
 import Navbar from './components/Navbar';
 function App() {
   return (
     <div >
       <Navbar />
+      <Login/>
+      {/* <SignUp/> */}
       {/* <Hero /> */}
 
     </div>
