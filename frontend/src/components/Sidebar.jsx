9d08be9cc84a8e38782c5054b11cbdd1949481a4
Elias-Velasquez <eliasv.cabj@gmail.com>
2022-07-22 12:01:42 -0500

    kito comments & token + category product

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -86,7 +86,7 @@ function Sidebar() {
                                     <path stroke="none" d="M0 0h24v24H0z" />
                                     <path d="M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1" />
                                 </svg>
-                                <span className="text-sm  ml-2">Productos</span>
+                                <span id="Producto" value="Producto" onClick={selectCategory} className="text-sm  ml-2">Productos</span>
                             </div>
                         </li>
                         <li className="list-hover flex w-full justify-between text-gray-600 hover:text-gray-300 cursor-pointer items-center px-8 py-3">
