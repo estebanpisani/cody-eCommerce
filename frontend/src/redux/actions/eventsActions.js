8e1735d95ea6ab1d1bce92b6c0819422cbf881a7
Esteban Pisani <estebandpisani@gmail.com>
2022-07-27 13:46:51 -0300

    Error de urls arreglado

--- frontend/src/redux/actions/eventsActions.js
+++ frontend/src/redux/actions/eventsActions.js
@@ -6,7 +6,7 @@ const eventsActions = {
     getEvents: () => {
         return async (dispatch, getState) => {
             try {
-                const res = await axios.get(`{URL_API}/api/events`)
+                const res = await axios.get(`${URL_API}/api/events`)
                 dispatch({ type: 'GET_EVENTS', payload: res.data.response.events })
             } catch (error) {
                 console.log(error)
@@ -15,7 +15,7 @@ const eventsActions = {
     },
     getEventById: (id) => {
         return async (dispatch, getState) => {
-            const res = await axios.get(`{URL_API}/api/events/${id}`)
+            const res = await axios.get(`${URL_API}/api/events/${id}`)
             // dispatch({ type: "GET_EVENT_BY_ID", payload: res.data.response.event })
             return res
 
@@ -24,7 +24,7 @@ const eventsActions = {
     modifyEvent: (_id, data) => {
         return async (dispatch, getState) => {
             try {
-                const res = await axios.put(`{URL_API}/api/events/${_id}`, data);
+                const res = await axios.put(`${URL_API}/api/events/${_id}`, data);
                 return res
             } catch (error) {
                 console.log(error)
@@ -35,7 +35,7 @@ const eventsActions = {
     addEvent: (name, description, authors, categories, images, price, limit, date, tags, attendance, likes, comments) => {
         return async (dispatch, getState) => {
             try {
-                await axios.post(`{URL_API}/api/events`, { name, description, authors, categories, images, price, limit, date, tags, attendance, likes, comments })
+                await axios.post(`${URL_API}/api/events`, { name, description, authors, categories, images, price, limit, date, tags, attendance, likes, comments })
             } catch (error) {
                 console.log(error)
             }
@@ -44,7 +44,7 @@ const eventsActions = {
     deleteEvent: (_id) => {
         return async (dispatch, getState) => {
             try {
-                await axios.delete(`{URL_API}/api/events/${_id}`)
+                await axios.delete(`${URL_API}/api/events/${_id}`)
             } catch (error) {
                 console.log(error)
             }
@@ -54,7 +54,7 @@ const eventsActions = {
         const token = localStorage.getItem('token')
         return async () => {
             try {
-                let response = await axios.put(`{URL_API}/api/events/like/${_id}`, {},
+                let response = await axios.put(`${URL_API}/api/events/like/${_id}`, {},
                     {
                         headers: {
                             Authorization: "Bearer " + token
@@ -70,7 +70,7 @@ const eventsActions = {
         const token = localStorage.getItem('token')
         return async () => {
             try {
-                let response = await axios.put(`{URL_API}/api/events/attendance/${_id}`, {},
+                let response = await axios.put(`${URL_API}/api/events/attendance/${_id}`, {},
                     {
                         headers: {
                             Authorization: "Bearer " + token
---
+++ frontend/src/url.js
@@ -0,0 +1,2 @@
+const URL_API = 'https://cody-coffee-api.herokuapp.com';
+export default URL_API;
