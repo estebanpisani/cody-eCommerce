b34fc4d1b86215cbaece5017b55a2bcf9f8c3fb2
Esteban Pisani <estebandpisani@gmail.com>
2022-09-11 18:10:51 -0300

    Haciendo modal responsive

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -5,18 +5,6 @@ import Modal from '@mui/material/Modal';
 import ProductItem from "./ProductItem";
 import '../styles/sidebar.css'
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 500,
-  bgcolor: '#f8b384',
-  border: '1px solid #581c0c',
-  boxShadow: 24,
-  p: 4,
-};
-
 export default function ProductCard({ data, functionReload }) {
 
   const [open, setOpen] = React.useState(false);
@@ -40,17 +28,14 @@ export default function ProductCard({ data, functionReload }) {
         <p className="text-sm font-medium text-gray-900 price my-1">${data.price}</p>
         <div className='detail-modal'></div>
         <button className="addToCart-button boton4" onClick={handleOpen}>Ver más</button>
+
         <Modal
           open={open}
           onClose={handleClose}
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description"
         >
-          <Box className='box-modal' sx={style}>
-            <div className='add-cart'>
               <ProductItem key={data._id} data={data} functionReload={ReloadChange} handleClose={handleClose} />
-            </div>
-          </Box>
         </Modal>
       </div>
     </>
--- frontend/src/components/ProductItem.js
+++ frontend/src/components/ProductItem.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
@@ -15,13 +16,13 @@ import 'react-toastify/dist/ReactToastify.css';
 const ProductItem = ({ data, functionReload, handleClose }) => {
 
   let { _id, name, price, description, image, stock, variations } = data;
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [opcion, setOpcion] = useState(10);
-  const [inputname, setInputname] = useState(name)
-  const [inputstock, setInputstock] = useState(stock)
-  const [inputprice, setInputprice] = useState(price)
+  const [inputname, setInputname] = useState(name);
+  const [inputstock, setInputstock] = useState(stock);
+  const [inputprice, setInputprice] = useState(price);
 
-  const user = useSelector(store => store.userReducer.user)
+  const user = useSelector(store => store.userReducer.user);
 
   const handleChange = (event) => {
     setOpcion(event.target.value);
@@ -37,7 +38,7 @@ const ProductItem = ({ data, functionReload, handleClose }) => {
         }
       })
     functionReload()
-  }
+  };
 
   async function modifyProduct(event) {
     const modifiedProduct = {
@@ -49,66 +50,68 @@ const ProductItem = ({ data, functionReload, handleClose }) => {
     await dispatch(productActions.modifyProduct(modifiedProduct))
     functionReload()
     handleClose()
-  }
+  };
 
   return (
-    <div className='more-border' style={{ padding: "1rem" }}>
-      <div className='more-product'>
-        <img
-          className='image-modal'
-          src={image}
-          alt="product"
-        />
+    <div className='box-modal w-4/5 sm:w-3/5 md:w-2/5 h-4/6 sm:h-5/6'>
+      <div className='more-border'>
+        <img className='image-modal' src={image} alt="product" />
         {user?.user.role === 'admin' ?
           <input type='textarea' defaultValue={name} value={inputname} onChange={(event) => setInputname(event.target.value)} className='more-name text-sm font-products' />
           :
           <h4 className="text-lg font-products">{name}</h4>
         }
-      </div>
-      {user?.user.role === 'admin' ?
-        <div className='more-buy'>
-          <p>Stock:</p>
-          <input type='textarea' defaultValue={stock} onChange={(event) => setInputstock(event.target.value)} className='more-stock' />
-          <p>Precio: </p>
-          <input type='textarea' defaultValue={price} onChange={(event) => setInputprice(event.target.value)} className='more-price'></input>
-        </div>
-        :
-        <div className='more-buy'>
-          <p className='more-data'>Disponible: {stock}</p>
-          <FormControl color='warning' className='more-select' sx={{ m: 1, minWidth: 120 }} size="small">
-            <InputLabel color='warning' id="demo-select-small">Variedades:</InputLabel>
-            <Select
-              color='warning'
-              labelId="demo-select-small"
-              id="demo-select-small"
-              value={opcion}
-              defaultValue={10}
-              label="Opcion"
-              onChange={handleChange}
-            >
-              <MenuItem color='warning' value={10}>{variations[0]}</MenuItem>
-              <MenuItem color='warning' value={20}>{variations[1]}</MenuItem>
-              <MenuItem color='warning' value={30}>{variations[2]}</MenuItem>
-            </Select>
-          </FormControl>
-          <h5 className='more-data'>${price}.00</h5>
-        </div>
-      }
 
-      {user?.user.role === 'admin' ?
-        <div className='admin-container'>
-          <h3 className='admin-title'>« Administrador »</h3>
-          <h3 className='admin-text'>¿Que cambios desea realizar en el producto?</h3>
-          <div >
-            <button className="  button-admin" onClick={modifyProduct}> Modificarlo <ModeEditIcon /></button>
-            <button className=" button-admin" onClick={deleteProduct}>Eliminarlo<DeleteIcon /></button>
-          </div>
+        <div className='flex flex-col lg:flex-row justify-around items-center w-full'>
+          {user?.user.role === 'admin' ?
+            <>
+              <p>Stock:</p>
+              <input type='textarea' defaultValue={stock} onChange={(event) => setInputstock(event.target.value)} className='more-stock' />
+              <p>Precio: </p>
+              <input type='textarea' defaultValue={price} onChange={(event) => setInputprice(event.target.value)} className='more-price'></input>
+            </>
+            :
+            <>
+              <div className='more-data my-2'>
+                <p>Disponible:</p>
+                <p>{stock} unidades</p>
+              </div>
+              <FormControl color='warning' className='more-select' sx={{ m: 2, minWidth: 120 }} size="small">
+                <InputLabel color='warning' id="demo-select-small">Variedades:</InputLabel>
+                <Select
+                  color='warning'
+                  labelId="demo-select-small"
+                  id="demo-select-small"
+                  value={opcion}
+                  defaultValue={10}
+                  label="Opcion"
+                  onChange={handleChange}
+                >
+                  <MenuItem color='warning' value={10}>{variations[0]}</MenuItem>
+                  <MenuItem color='warning' value={20}>{variations[1]}</MenuItem>
+                  <MenuItem color='warning' value={30}>{variations[2]}</MenuItem>
+                </Select>
+              </FormControl>
+              <p className='more-data my-2'>${price}.00</p>
+            </>
+          }
         </div>
-        : user?.user.role === 'user' ?
-          <button className="addToCart-button boton3" onClick={() => dispatch(shoppingActions.addToCart(data._id))}>Agregar</button>
-          :
-          <p className="text-base font-products">Inicia sesión para agregar este producto a tu pedido</p>
-      }
+
+        {user?.user.role === 'admin' ?
+          <div className='admin-container'>
+            <h3 className='admin-title'>« Administrador »</h3>
+            <h3 className='admin-text'>¿Que cambios desea realizar en el producto?</h3>
+            <div >
+              <button className="  button-admin" onClick={modifyProduct}> Modificarlo <ModeEditIcon /></button>
+              <button className=" button-admin" onClick={deleteProduct}>Eliminarlo<DeleteIcon /></button>
+            </div>
+          </div>
+          : user?.user.role === 'user' ?
+            <button className="addToCart-button boton3" onClick={() => dispatch(shoppingActions.addToCart(data._id))}>Agregar</button>
+            :
+            <p className="text-base font-products">Inicia sesión para agregar este producto a tu pedido</p>
+        }
+      </div>
     </div>
   );
 
